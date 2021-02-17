5215cc7b78f2ba7c3205b78004acfa6f96df8e9f
Troglody <troglodyxplays0@gmail.com>
2021-02-17 17:36:33 +0100

    refacto(front)

--- static/WebService.js
+++ static/WebService.js
@@ -1,123 +1,127 @@
 const GetStatus = () => {
-    $('#HaStatus').css('background-color', 'orange');
-    $.ajax({
-        type: 'GET',
-        url: '/webService',
-        data: {
-            'do': 'GET_HA_Status'
-        },
-        dataType: 'JSON',
-        success: (result) => {
-            var state = result.message;
-            console.log(state);
-            $('#HaStatus').css('background-color', state.includes('API running') ? 'green' : 'red');
-        },
-        error: () => {
-            console.log("Home Assistant is unreachable");
-        }
-    })
-}
+  $("#HaStatus").css("background-color", "orange");
+  $.ajax({
+    type: "GET",
+    url: "/webService",
+    data: {
+      do: "GET_HA_Status",
+    },
+    dataType: "JSON",
+    success: (result) => {
+      var state = result.message;
+      console.log(state);
+      $("#HaStatus").css(
+        "background-color",
+        state.includes("API running") ? "green" : "red"
+      );
+    },
+    error: () => {
+      console.log("Home Assistant is unreachable");
+    },
+  });
+};
 
 const LightToggle = () => {
-    $.ajax({
-        type: 'GET',
-        url: '/webService',
-        data: {
-            'do': 'POST_LightToggle'
-        },
-        dataType: 'JSON',
-        success: (result) => {
-            console.log(`Lampe ${result[0].state == 'on' ? 'allumée' : 'éteinte'}`); //PUSH NOTIF
-        },
-        error: () => {
-            console.log("erreur webservice");
-        }
-    })
-}
-
+  $.ajax({
+    type: "GET",
+    url: "/webService",
+    data: {
+      do: "POST_LightToggle",
+    },
+    dataType: "JSON",
+    success: (result) => {
+      console.log(`Lampe ${result[0].state == "on" ? "allumée" : "éteinte"}`); //PUSH NOTIF
+    },
+    error: () => {
+      console.log("erreur webservice");
+    },
+  });
+};
 
 const LightOn = () => {
-    $.ajax({
-        type: 'POST',
-        url: '/webService',
-        data: JSON.stringify({
-            'do': 'POST_LightOn',
-            'params': {
-                'lampID': 'light.led_esp8266',
-                'brightness': 255,
-                'color': 'red'
-            }
-        }),
-        contentType: 'application/json',
-        dataType: 'json',
-        success: (result) => {
-            const emptyArray = Array.isArray(result) && result.length == 0;
-            emptyArray ? console.log('Lampe déja allumée') : console.log(`Lampe ${result[0].state == 'on' ? 'allumée' : 'éteinte'}`); //PUSH NOTIF
-        },
-        error: (result) => {
-            console.log("erreur webservice", result);
-        }
-    })
-}
+  $.ajax({
+    type: "POST",
+    url: "/webService",
+    data: JSON.stringify({
+      do: "POST_LightOn",
+      params: {
+        lampID: "light.led_esp8266",
+        brightness: 255,
+        color: "red",
+      },
+    }),
+    contentType: "application/json",
+    dataType: "json",
+    success: (result) => {
+      const emptyArray = Array.isArray(result) && result.length == 0;
+      emptyArray
+        ? console.log("Lampe déja allumée")
+        : console.log(
+            `Lampe ${result[0].state == "on" ? "allumée" : "éteinte"}`
+          ); //PUSH NOTIF
+    },
+    error: (result) => {
+      console.log("erreur webservice", result);
+    },
+  });
+};
 
 const GetLampId = () => {
-    $.ajax({
-        type: 'POST',
-        url: '/webService',
-        data: JSON.stringify({
-            'do': 'GET_ObjectbyId',
-            'params': {
-                'id': 'light.led_esp8266'
-            }
-        }),
-        contentType: 'application/json',
-        dataType: 'JSON',
-        success: (result) => {
-            console.log(result.entity_id, result.state); //PUSH NOTIF
-        },
-        error: (result) => {
-            console.log("erreur webservice", result); //PUSH NOTIF
-        }
-    })
-}
+  $.ajax({
+    type: "POST",
+    url: "/webService",
+    data: JSON.stringify({
+      do: "GET_ObjectbyId",
+      params: {
+        id: "light.led_esp8266",
+      },
+    }),
+    contentType: "application/json",
+    dataType: "JSON",
+    success: (result) => {
+      console.log(result.entity_id, result.state); //PUSH NOTIF
+    },
+    error: (result) => {
+      console.log("erreur webservice", result); //PUSH NOTIF
+    },
+  });
+};
 
 const POST_Notification = (title, message) => {
-    $.ajax({
-        type: 'POST',
-        url: '/webService',
-        data: JSON.stringify({
-            'do': 'POST_Notification',
-            'params': {
-                'message': message,
-                'title': title
-            }
-        }),
-        contentType: 'application/json',
-        dataType: 'JSON',
-        success: (result) => {
-            console.log(result); //PUSH NOTIF
-        },
-        error: (result) => {
-            console.log("erreur webservice", result); //PUSH NOTIF
-        }
-    })
-}
-
+  $.ajax({
+    type: "POST",
+    url: "/webService",
+    data: JSON.stringify({
+      do: "POST_Notification",
+      params: {
+        message: message,
+        title: title,
+      },
+    }),
+    contentType: "application/json",
+    dataType: "JSON",
+    success: (result) => {
+      console.log(result); //PUSH NOTIF
+    },
+    error: (result) => {
+      console.log("erreur webservice", result); //PUSH NOTIF
+    },
+  });
+};
 
 const GetAllObjects = () => {
-    return $.ajax({
-        type: 'GET',
-        url: '/webService',
-        data: {
-            'do': 'GET_API_States'
-        },
-        dataType: 'JSON',
-        error: (result) => {
-            console.log("erreur webservice", result); //PUSH NOTIF
-        }
-    })
-}
-
+  return $.ajax({
+    type: "GET",
+    url: "/webService",
+    data: {
+      do: "GET_API_States",
+    },
+    dataType: "JSON",
+    error: (result) => {
+      console.log("erreur webservice", result); //PUSH NOTIF
+    },
+  });
+};
 
 /*
 BasicPost = () => {
--- static/main.js
+++ static/main.js
@@ -1,80 +1,106 @@
 $(document).ready(function () {
+  GetStatus();
+  fillObjectList();
+  // const t = setInterval(fillObjectList, 5000);
 
+  $("#myTab a").on("click", function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+
+  $("#HaStatus").on("click", () => {
     GetStatus();
-    fillObjectList();
-    // const t = setInterval(fillObjectList, 5000);
+  });
 
-    $('#myTab a').on('click', function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    })
+  $("#iLogo").on("click", () => {
+    LightToggle();
+  });
 
+  $("#LightOn").on("click", () => {
+    LightOn();
+  });
 
-    $('#HaStatus').on('click', () => {
-        GetStatus();
-    })
-
-    $('#iLogo').on('click', () => {
-        LightToggle();
-    })
-
-    $('#LightOn').on('click', () => {
-        LightOn();
-    })
-
-    $('#LampId').on('click', () => {
-        GetLampId();
-    })
-
-
-})
-
+  $("#LampId").on("click", () => {
+    GetLampId();
+  });
+});
 
 const fillObjectList = async () => {
+  let objList = {};
+  $("#objectsList").empty();
+  const Objects = await GetAllObjects();
+  // Automation(Objects);
+  $.each(Objects, function (k, object) {
+    const objName = object.attributes.friendly_name;
+    let properties = object.state;
 
-    let objList = {}
-    $('#objectsList').empty();
-    const Objects = await GetAllObjects();
+    if (properties) {
+      objList[k] = {
+        name: objName,
+        properties: properties,
+      };
+    }
 
-    $.each(Objects, function (k, object) {
-        const objName = object.attributes.friendly_name;
-        let properties = object.state;
+    let dataPills = `: <span class="badge badge-pill badge-dark">${properties}</span>`;
 
-        if (properties) {
-            objList[k] = {
-                'name': objName,
-                'properties': properties
-            }
+    if (tryParseJSON(properties)) {
+      properties = tryParseJSON(properties);
+      dataPills = "";
+      $.each(properties, function (k, v) {
+        if (typeof v == "object") {
+          v = Object.values(v)[0];
         }
-
-        let dataPills;
-        if (tryParseJSON(properties)) {
-            properties = tryParseJSON(properties)
-            $.each(properties, function (k, v) {
-                if (typeof (v) == "object") {
-                    v = Object.values(v)[0];
-                }
-                if (k == 'at') {
-                    k = 'le';
-                    const r = v.slice(19, v.length - 6);
-                    v = new Date(v.replace(r, '')).toLocaleString();
-                }
-                dataPills = `<span class="badge badge-pill badge-dark">${k} : <span class="badge badge-pill badge-light">${v}</span></span>`;
-            })
+        if (k == "at") {
+          k = "le";
+          const r = v.slice(19, v.length - 6);
+          v = new Date(v.replace(r, "")).toLocaleString();
         }
-        $('#objectsList').append(`<li class="list-group-item ${k}">${objName} ${dataPills}<div class="statusColor mt-1 float-right"></div></li>`)
-        $(`#objectsList .${k} .statusColor`).css('background-color', properties ? 'green' : 'red');
-    });
-    return objList;
-}
-
+        dataPills += `<span class="badge badge-pill badge-dark">${k} : <span class="badge badge-pill badge-light">${v}</span></span>, `;
+      });
+    }
+    $("#objectsList").append(
+      `<li class="list-group-item ${k}">${objName} ${dataPills}<div class="statusColor mt-1 float-right"></div></li>`
+    );
+    $(`#objectsList .${k} .statusColor`).css(
+      "background-color",
+      properties ? "green" : "red"
+    );
+  });
+  return objList;
+};
 
 function tryParseJSON(jsonString) {
-    try {
-        var o = JSON.parse(jsonString);
-        if (o && typeof o === "object") {
-            return o;
-        }
-    } catch (e) {}
-    return false;
+  try {
+    var o = JSON.parse(jsonString);
+    if (o && typeof o === "object") {
+      return o;
+    }
+  } catch (e) {}
+  return false;
+}
+
+const Automation = (Objects) => {
+  $.each(Objects, function (k, object) {
+    if (object.attributes.friendly_name == "AC_Sensor") {
+      let data = JSON.parse(object.state);
+      let temp = data.Temperature.temp;
+
+      switch (true) {
+        case temp > 25:
+          //openfan
+          break;
+
+        case temp > 45:
+          //notif danger
+          break;
+
+        case temp < 25:
+          //closeFan
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
 };
